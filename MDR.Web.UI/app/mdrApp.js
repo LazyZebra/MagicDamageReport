e82399e871d2989c10580d70936d181570f2dd60
Salty Petrov <a.object13@gmail.com>
2015-10-31 14:08:50 -0400

    added a bunch of angular libs and updated the navbar

---
+++ MDR.Web.UI/app/mdrApp.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("mdrApp", ['ngRoute'])
+    .config(routeConfig);
+
+    function routeConfig($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'app/damageReport/damageReport.html',
+                controller: 'damageReportController'
+            }).
+            when('/contact', {
+                templateUrl: 'app/contact/contact.html',
+                controller: 'contactController'
+            })
+            .otherwise({ redirectTo: '/' });
+    };
+})();
