dc20f2609c80558341786bca63f2f737f126978f
Salty Petrov <a.object13@gmail.com>
2015-11-09 00:16:28 -0500

    Added UI and API projects

---
+++ MDR.DataAccess/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MDR.DataAccess
+{   
+    //Petrov: I will not be implementing IDisposable because I'm working with DbContext
+    //which already implements it out of the box
+    public interface IRepository<T>
+    {
+        void Add(T entity);
+        void Remove(T entity);
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ MDR.DataAccess/Repositories/DamageReportRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+using MDR.Domain.Model;
+
+namespace MDR.DataAccess.Repositories
+{
+    public class DamageReportRepository : IRepository<DamageReport> 
+    {
+        public void Add(DamageReport entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(DamageReport entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<DamageReport> Find(Expression<Func<DamageReport, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
